c38c19906e5735a3b6239ec2dd78ef60821d9508
Megan Kaiser <meganrayanne@yahoo.com>
2018-04-10 16:50:48 -0600

    Add images folder and place background image till design finalized, update title to weathrly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { TodaysWeather } from './Data-interator'
 import CurrentWeather from './CurrentWeahter'
