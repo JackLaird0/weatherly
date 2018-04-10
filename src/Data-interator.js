d5d285b26e7f7dac85109584ecd286f176908e55
Jack Laird <Jack.laird0@gmail.com>
2018-04-10 15:13:08 -0600

    Create a class that iterates through the data

---
+++ src/Data-interator.js
@@ -0,0 +1,14 @@
+import Data from './mock-data';
+
+class TodaysWeather {
+  constructor() {
+    this.location = Data.current_observation.display_location.full;
+    this.currentTemp = Data.current_observation.temp_f;
+    this.high = Data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    this.low = Data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+    this.day = Data.forecast.simpleforecast.forecastday[0].date.pretty;
+    this.description = Data.forecast.simpleforecast.forecastday[0].conditions;
+  }
+}
+
+export  {TodaysWeather}
