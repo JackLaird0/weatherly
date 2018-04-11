b70f6d38d3d4024d330e99ecd2958d6023457010
Jack Laird <Jack.laird0@gmail.com>
2018-04-11 14:02:24 -0600

    Create map function to make multiple weather cards

--- src/TenDay.js
+++ src/TenDay.js
@@ -1,1 +1,21 @@
 import React from 'react';
+
+const TenDay = props => {
+  console.log(props)
+  //   return null
+    return (
+      <div> {
+      props.TenDay.map( (daily, index) => {
+      return (
+        <div className="weatherCard">
+          <h2> {daily.day} </h2>
+          <img src={daily.weatherIcon} />
+          <h2> {daily.weather} </h2>
+        </div>
+      )})
+    }
+      </div>
+      )
+  }
+  
+  export default TenDay
