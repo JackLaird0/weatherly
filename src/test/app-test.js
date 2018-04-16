61cfdf2a7fdc351c43eeaa07ce63575a980bbf3d
Megan Kaiser <meganrayanne@yahoo.com>
2018-04-16 10:26:43 -0600

    Start tests and set up test

---
+++ src/test/app-test.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import App from './App';
+
+// import { shallow } from 'enzyme';
+
+// describe('App', () => {
+//   it('should be a thing', () => {
+//     console.log('hello')
+//     const wrapper = shallow(<App />)
+//     expect(wrapper).toBeDefined()
+//   });
+// });
