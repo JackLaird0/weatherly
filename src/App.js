d807222be713cbcaf552ad2d113795c7847e1455
Jack Laird <Jack.laird0@gmail.com>
2018-04-16 09:54:57 -0600

    Create welcome screen and render on page load

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './Styles/App.css';
-import { TodaysWeather, SevenHourForecast, TenDayForecast } from './Data-iterator'
-import CurrentWeather from './CurrentWeather'
+import { TodaysWeather, SevenHourForecast, TenDayForecast } from './Data-iterator';
+import CurrentWeather from './CurrentWeather';
 import SevenHour from './SevenHour';
 import Data from './mock-data';
-import Search from './Search'
-import TenDay from './TenDay'
-
-
-
-// const sevenHourWeather = new SevenHourForecast();
+import Search from './Search';
+import TenDay from './TenDay';
+import Welcome from './Welcome';
 
 class App extends Component {
   constructor() {
@@ -19,30 +15,44 @@ class App extends Component {
     this.state = {
       todaysWeather: TodaysWeather(Data),
       SevenHourForecast: SevenHourForecast(Data),
-      TenDayForecast: TenDayForecast(Data)
+      TenDayForecast: TenDayForecast(Data),
+      WelcomeScreen: false
     }
   }
-
+  
   render() {
-    return (
-      <div className="App">
-        <div className="top-container">
-          <div className="search-current">
-            <Search />
-            <CurrentWeather   
-                location={this.state.todaysWeather.location}
-                day={this.state.todaysWeather.day}
-                currentTemp={this.state.todaysWeather.currentTemp}
-                high={this.state.todaysWeather.high}
-                low={this.state.todaysWeather.low}
-                description={this.state.todaysWeather.description}
-                weatherIcon={this.state.todaysWeather.weatherIcon} />
-          </div>
-          <SevenHour
-              SevenHour={this.state.SevenHourForecast} />
+    const showWeather =
+    <div>
+      <div className="top-container">
+        <div className="search-current">
+          <Search />
+          <CurrentWeather   
+              location={this.state.todaysWeather.location}
+              day={this.state.todaysWeather.day}
+              currentTemp={this.state.todaysWeather.currentTemp}
+              high={this.state.todaysWeather.high}
+              low={this.state.todaysWeather.low}
+              description={this.state.todaysWeather.description}
+              weatherIcon={this.state.todaysWeather.weatherIcon} />
         </div>
+        <SevenHour
+            SevenHour={this.state.SevenHourForecast} />
+      </div>
+      <div className="tenday-forecast">
         <TenDay 
         TenDay={this.state.TenDayForecast} />  
+      </div>
+    </div>
+
+    const showWelcome =
+    <div className="Welcome-screen">
+      <Welcome />
+      <Search />
+    </div>
+
+    return (
+      <div className="App">
+       {this.state.WelcomeScreen ? showWelcome : showWeather}
       </div>
     );
   }
--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Styles/search.css'
 
-const Search = () => {
-  return (
-    <div>
-      <input type="text" placeholder="Search for your city" className="search-input"/>
-      <button className="submit-button"> Submit </button>
-    </div>
-  )
+class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cityData: '',
+      cityArray: []
+    }
+
+    this.saveUserInput = this.saveUserInput.bind(this);
+    this.saveCity = this.saveCity.bind(this);
+  }
+
+  saveCity(e) {
+    this.setState({cityArray: []})
+    this.setState({cityArray: [this.state.cityData]})
+    e.preventDefault();
+  }
+
+  saveUserInput(e) {
+    this.setState({cityData: e.target.value});
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Search for your city" className="search-input" onChange={this.saveUserInput} /> 
+        <button className="submit-button" onClick={this.saveCity}> Submit </button>
+      </div>
+    )
+  }
 }
 
 export default Search
